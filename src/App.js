17ad4b8a867dfd042e9436a0807f12211979093d
JesusFernandez86 <antoniofarroyo86@gmail.com>
2020-01-05 09:03:03 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Car from "./components/Car";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Car />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Car.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react"; //importamos useState para poder cambiar los estados
+
+export default function Car() {
+  //primero indicamos el nombre de la variable y despues el nombre de la funcion que va a cambiar el estado
+  const [started, setStarted] = useState(false);
+  const [countKms, setKms] = useState(0);
+
+  const checkStatus = () => {
+    if (!started) {
+      return <span style={{ color: "green" }}>On</span>;
+    } else {
+      return <span style={{ color: "red" }}>Off</span>;
+    }
+  };
+
+  const increaseKms = value => {
+    if (!started) {
+      setKms(countKms + value);
+    } else {
+      alert("the car is off");
+    }
+  };
+
+  return (
+    <div>
+      <h2> Our car is: {checkStatus()} </h2>
+      <h2>Kilometers: {countKms} kms</h2>
+      <button
+        //   para cambiar el estado creamos una funcion flecha anonima que invoque a la funcion que hemos creado antes para cambiar el estado
+        //   y le pasamos los argumentos que queramos
+        onClick={() => {
+          setStarted(!started);
+        }}
+      >
+        On / Off
+      </button>
+      <button
+        onClick={() => {
+          increaseKms(5);
+        }}
+      >
+        Increase kms
+      </button>
+    </div>
+  );
+}
